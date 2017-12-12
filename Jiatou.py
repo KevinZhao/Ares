3e0bb04d6ef64c89da866c157928af07fead196c
Kevin Zhao <kevin8093@126.com>
2017-12-12 22:21:10 +0800

    up

--- Jiatou.py
+++ Jiatou.py
@@ -1,56 +1,54 @@
 # -*- coding:utf-8 -*- 
 from src.HK_data_miner import *
-from src.repeatTimer import *
+from src.util import *
 from src.Tushare_adapter import *
 from src.HK_data_processing import *
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
 
 def get_HK_detail():
 
-	if miner._detail_downloaded == False:
-    
-		miner.get_current_data()
-		miner.write_to_mysqldb()
-		update_HK_detail()
+	#download
+	miner.get_current_data()
+	miner.write_to_mysqldb()
 
-		print(miner._detail_downloaded)
+	#update
+	data_proc = HK_data_processing()
+	data_proc.update_HK_detail()
 
-def store_history_data(no):
-    #历史交易数据
-    code = no_to_code(no)
-    
-    ts_adapter = Tushare_adapter()
-    ts_adapter.update_history_price(code)
+	pass
 
+def get_HK_amount_flow():
+	print('get_HK_amount_flow not implemented')
 
-def update_HK_detail():
-
-	# read from database
-	db = Jiatou_DB()._db
-
-	#港股通代码转换
-	resultProxy=db.execute(text('select distinct no from tb_daily'))
-	no_result = resultProxy.fetchall()
-	result_array= []
-
-	#循环遍历求差值
-	for no in no_result:
-	    
-	    store_history_data(no[0])
-
-	    print('processing code:', no[0])
-	    data_processor = HK_data_processing()
-	    data_processor.update_amount(no[0])
-	    data_processor.update_amount_diff(no[0])
-
+def get_HK_amount_top():
+	print('get_HK_amount_flow not implemented')
 
 miner = HK_data_miner()
+sched = BackgroundScheduler()
 
-r = RepeatTimer(60.0, get_HK_detail)
-r.start()
+#周二至周六对应 1点执行 获取每日持仓情况, 执行一次
+sched.add_job(
+	get_HK_detail, 'cron', 
+	hour='1', 
+	day_of_week ='2-6', 
+	timezone = 'Asia/Shanghai')
 
-#获取时间：
-#当日是周二至周六时，获取前一日详细数据
+#周一至周五对应 日9点半至11点半，13点至15点，获取资金流入流出，每秒执行一次
+sched.add_job(
+	get_HK_amount_flow, 'cron', 
+	hour='09-15', 
+	minute='0-59', 
+	second = '0-59', 
+	timezone = 'Asia/Shanghai')
 
+#周一至周五对应 日5点40分，获取资金流入流出，执行一次
+sched.add_job(
+	get_HK_amount_top, 'cron', 
+	hour='5', 
+	minute='40-45', 
+	timezone = 'Asia/Shanghai')
 
-#当日是周一至周五时，获取陆股通实时资金数据，当日晚5点40分后，获取十大成交数据
+sched.start()
+
 
